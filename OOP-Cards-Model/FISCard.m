315b69b1cc3d9f8fd1166ebd44ecddf4572664d5
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-01 12:06:26 -0500

    Implements +[FISCard validRanks].

--- OOP-Cards-Model/FISCard.m
+++ OOP-Cards-Model/FISCard.m
@@ -24,7 +24,7 @@
 
 + (NSArray *)validRanks {
     
-    return nil;
+    return @[@"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
 }
 
 - (instancetype)initWithSuit:(NSString *)suit rank:(NSString *)rank {
